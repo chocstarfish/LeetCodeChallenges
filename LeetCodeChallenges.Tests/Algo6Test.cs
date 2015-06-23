72562392b826a76a15003e26ad7085cc0d30738e
Nicolas Zhang <nicolasloco@gmail.com>
2015-06-23 13:43:35 -0400

    finished 6 - ZigZag Conversion challenge

---
+++ LeetCodeChallenges.Tests/Algo6Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeetCodeChallenges.Tests
+{
+    [Trait("Challenge", "Algo6")]
+    public class Algo6Test
+    {
+        [Fact(DisplayName = "Add Two Numbers Passing")]
+        public void PassingTest()
+        {
+            Algo6 algo6 = new Algo6();
+            var test1 = "BOSTONISAREALLYNICECITY";
+            /*
+             * B     I     L     E 
+             * O   N S   A L   C C
+             * S O   A E   Y I   I Y
+             * T     R     N     T
+             */
+            var expected1 = "BILEONSALCCSOAEYIIYTRNT";
+            var actual1 = algo6.Convert(test1, 4);
+            Assert.Equal(expected1, actual1);
+
+            var test2 = "PAYPALISHIRING";
+            var expected2 = "PAHNAPLSIIGYIR";
+            Assert.Equal(expected2, algo6.Convert(test2, 3));
+        }
+
+        [Fact(DisplayName = "Add Two Numbers Failing")]
+        public void FailingTest()
+        {
+
+
+        }
+    }
+}
---
+++ LeetCodeChallenges/Algo6.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeChallenges
+{
+    /// <summary>
+    /// ZigZag Conversion
+    /// 
+    /// The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this:
+    ///  (you may want to display this pattern in a fixed font for better legibility)
+    /// P   A   H   N
+    /// A P L S I I G
+    /// Y   I   R
+    /// And then read line by line: "PAHNAPLSIIGYIR"
+    /// Write the code that will take a string and make this conversion given a number of rows:
+    /// string convert(string text, int nRows);
+    /// convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR". 
+    /// </summary>
+    public class Algo6
+    {
+        public string Convert(string s, int numRows)
+        {
+            if (numRows == 1)
+            {
+                return s;
+            }
+
+            string result = string.Empty;
+            var skip = numRows * 2 - 2;
+            for (var i = 0; i < numRows; i++)
+            {
+                for (var j = i; j < s.Length; j += skip)
+                {
+                    result += s[j];
+                    var index = j + skip - 2 * i;
+                    if (i != 0 && i != numRows - 1 && index < s.Length)
+                    {
+                        result += s[index];
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+
+}
